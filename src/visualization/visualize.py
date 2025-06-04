8d881ab6e090bb214fc28ebd4249d4afb4b59431
Carlos <carlosprovisorio@icloud.com>
2025-06-04 13:37:06 -0400

    Loop over all combinations and export for both sensors and generate and save the reports under the reports folder

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -156,3 +156,33 @@ ax[1].set_xlabel("samples")
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
 # --------------------------------------------------------------
+
+labels = data_frame["label"].unique()
+participants = data_frame["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        combined_plot_df = (
+            data_frame.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+
+        if len(combined_plot_df) > 0:
+
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+            combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+            ax[0].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].legend(
+                loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True
+            )
+            ax[1].set_xlabel("samples")
+
+            plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+            plt.show()
+
+
